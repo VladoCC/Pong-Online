22d145ab164b026ddae94b6492d7d3d1362b1ee6
VladoCC <vladoccplaym@gmail.com>
2020-11-05 19:19:49 +1000

    fixed server package's build.gradle

--- server/src/com/inkostilation/pong/server/ServerLauncher.java
+++ server/src/com/inkostilation/pong/server/ServerLauncher.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.inkostilation.pong.PongMain;
 
-public class DesktopLauncher {
+public class ServerLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new PongMain(), config);
