8a8e14aa7a1f1ea52705bfc78a087f7e9e899ca5
VladoCC <vladoccplaym@gmail.com>
2020-12-02 23:24:57 +1000

    Update PlayerData.java

--- core/src/com/inkostilation/pong/engine/PlayerData.java
+++ core/src/com/inkostilation/pong/engine/PlayerData.java
@@ -5,4 +5,25 @@ public class PlayerData {
     private Game game;
     private PlayerRole playerRole;
 
+
+    public PlayerData(Game game, PlayerRole playerRole) {
+        this.game = game;
+        this.playerRole = playerRole;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public PlayerRole getPlayerRole() {
+        return playerRole;
+    }
+
+    public void setPlayerRole(PlayerRole playerRole) {
+        this.playerRole = playerRole;
+    }
 }
